3392af94e1d2c4dc0e17954809fbc8a39b8aeb52
jeevankms <tve21cs065@cet.ac.in>
2024-12-01 20:13:50 +0530

    Auto commit on Sunday 01 December 2024 08:13:50 PM IST

--- Desktop/notes/theEditor/theEditor.c
+++ Desktop/notes/theEditor/theEditor.c
@@ -62,15 +62,22 @@ void abFree(struct abuf *ab){
   free(ab->b);
 }
 
-void editorDrawRows(struct abuf *ab){
+void editorDrawRows(struct abuf *ab) {
   int y;
-  for(y=0;y<E.screenrows;y++){
-    abAppend(ab, "~", 1);
-
-    if (y < E.screenrows - 1) {
-      abAppend(ab, "\r\n", 1);
+  for (y = 0; y < E.screenrows; y++) {
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+        "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
     }
-
+    abAppend(ab, "\x1b[K", 3);
+    if (y < E.screenrows - 1) {
+      abAppend(ab, "\r\n", 2);
+    }
   }
 }
 
