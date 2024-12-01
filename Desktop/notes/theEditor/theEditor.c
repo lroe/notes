86bcef9ed6b9c3e4aed5485f9044e1800108dfce
jeevankms <tve21cs065@cet.ac.in>
2024-12-01 19:37:07 +0530

    Auto commit on Sunday 01 December 2024 07:37:07 PM IST

--- Desktop/notes/theEditor/theEditor.c
+++ Desktop/notes/theEditor/theEditor.c
@@ -14,6 +14,8 @@ struct termios orig_termios;
 
 //terminal
 void die(const char *s) {
+    write(STDOUT_FILENO,"\x1b[2J",4);
+  write(STDOUT_FILENO, "\x1b[H",3);
   perror(s);
   exit(1);
 }
@@ -38,6 +40,7 @@ void enableRawMode() {
 //output
 void editorRefreshScreen(){
   write(STDOUT_FILENO,"\x1b[2J",4);
+  write(STDOUT_FILENO, "\x1b[H",3);
 }
 //input
 char editorReadKey(){
@@ -53,6 +56,8 @@ void editorProcessKeyPress(){
   char c = editorReadKey();
   switch(c){
   case CTRL_KEY('q'):
+    write(STDOUT_FILENO,"\x1b[2J",4);
+   write(STDOUT_FILENO, "\x1b[H",3);
     exit(0);
     break;
   }
